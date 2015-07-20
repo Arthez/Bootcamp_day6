c0839f5166b2188aeb6aa5f5e9b152edf7d894a3
Artur Ratowski <arturatow@gmail.com>
2015-07-20 17:53:17 +0200

    Bootcamp day 6.

--- app/src/main/java/pl/droidsonroids/letmetakeaselfie/ui/MainActivity.java
+++ app/src/main/java/pl/droidsonroids/letmetakeaselfie/ui/MainActivity.java
@@ -2,8 +2,6 @@ package pl.droidsonroids.letmetakeaselfie.ui;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,12 +12,10 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
